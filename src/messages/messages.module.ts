fdf65905137c27cf1db734de73d1194b675826b9
Park Min Hyeok <opensource@pmh.codes>
2022-04-17 14:31:56 +0900

    feat: impl createMessage

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -6,9 +6,18 @@ import { MessagesService } from './messages.service'
 import { MessagesController } from './messages.controller'
 import { AuthModule } from 'src/auth/auth.module'
 import { PermissionsModule } from 'src/permissions/permissions.module'
+import { CategoriesModule } from 'src/categories/categories.module'
+import { UsersModule } from 'src/users/users.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Messages]), AuthModule, PermissionsModule],
+  imports: [
+    TypeOrmModule.forFeature([Messages]),
+    AuthModule,
+    CategoriesModule,
+    PermissionsModule,
+    UsersModule,
+    CategoriesModule
+  ],
   controllers: [MessagesController],
   providers: [MessagesService]
 })
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { CategoriesService } from 'src/categories/categories.service'
+import { UsersService } from 'src/users/users.service'
 import { Repository } from 'typeorm'
 import { Messages, MessageType } from './messages.entity'
 
 @Injectable()
 export class MessagesService {
   private messagesRepository: Repository<Messages>
+  private categoriesService: CategoriesService
+  private usersService: UsersService
 
-  constructor (@InjectRepository(Messages) messagesRepository: Repository<Messages>) {
+  constructor (
+    @InjectRepository(Messages)
+      messagesRepository: Repository<Messages>,
+      categoriesService: CategoriesService
+  ) {
     this.messagesRepository = messagesRepository
+    this.categoriesService = categoriesService
   }
 
   public queryMessages (page = 0, perPage = 10, filter?: {
@@ -23,9 +32,22 @@ export class MessagesService {
     })
   }
 
-  public async createMessage (type: MessageType, subcategory: number, user: string) {
-    // TODO impl
-    return 1
+  public async createMessage (type: MessageType, subcategory: number, userId: string) {
+    const category =
+      await this.categoriesService.getSubcategory(subcategory)
+
+    const user =
+      type === 'submitted'
+        ? category.parent.manageUser
+        : await this.usersService.getUser(userId)
+
+    const data = await this.messagesRepository.insert({
+      type,
+      userId: user.userId,
+      requestedAt: Date.now()
+    })
+
+    return data.generatedMaps[0].notiId
   }
 
   public getMessage (id: number): Promise<Messages> {
